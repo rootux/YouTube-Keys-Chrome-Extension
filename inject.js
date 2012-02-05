79b285c77ea40448169eaa56e8f74c6903dbb4ca
rootux <galbra@gmail.com>
2012-02-05 21:12:30 +0200

    fixed memeber calls - should work now

--- inject.js
+++ inject.js
@@ -1,46 +1,48 @@
 /*globals chrome*/
 (function($, global, undefined) {
     var settings = {
-		playlistCurrentVideo: '.playlist-bar-item-playing',
-		mediaMap: {
-			next      : 176,
-			prev      : 177,
-			stop      : 178,
-			playPause : 179,
-			debugKey  : 77 /* lower case 'm' */ 
-		}
-	}, 
-	redirect  = function(url) {
-		chrome.extension.sendRequest({redirect: url});
-	},
-	nextVideo = function() {
-		var nextUrl = $(settings.playlistBar).next().find('a').attr('href');
-    	redirect(nextUrl);
-	},
-	prevVideo = function() {
-		var prevUrl = $(settings.playlistBar).prev().find('a').attr('href');
-    	redirect(prevUrl);
-	},
-	stopVideo = function() {},
-	playVideo = function() {};
-	
-	$('window').keyUp(function(event) {
-		var keyCode = event.keyCode;
-		switch(keyCode) {
-			case settings.debugKey: 
-			break;
-			case settings.playPause:
-				playVideo();
-			break;
-			case settings.stop:
-				stopVideo();
-			break;
-			case settings.next:
-				nextVideo();
-			break;
-			case settings.prev:
-				prevVideo();
-			break;
-		}
-	});
+    playlistCurrentVideo: '.playlist-bar-item-playing',
+    mediaMap: {
+      next      : 176,
+      prev      : 177,
+      stop      : 178,
+      playPause : 179,
+      debugKey  : 77 /* 'm' key for debugging keyboards without media keys */ 
+    }
+  }, 
+  redirect  = function(url) {
+    chrome.extension.sendRequest({redirect: url});
+  },
+  nextVideo = function() {
+    var nextUrl = $(settings.playlistCurrentVideo).next().find('a').attr('href');
+    if(nextUrl)
+        redirect(nextUrl);
+  },
+  prevVideo = function() {
+    var prevUrl = $(settings.playlistCurrentVideo).prev().find('a').attr('href');
+    if(prevUrl)
+        redirect(prevUrl);
+  },
+  stopVideo = function() {},
+  playVideo = function() {};
+  
+  $(window).keyup(function(event) {
+    var keyCode = event.keyCode;
+    switch(keyCode) {
+      case settings.mediaMap.debugKey:
+      break;
+      case settings.mediaMap.playPause:
+        playVideo();
+      break;
+      case settings.mediaMap.stop:
+        stopVideo();
+      break;
+      case settings.mediaMap.next:
+        nextVideo();
+      break;
+      case settings.mediaMap.prev:
+        prevVideo();
+      break;
+    }
+  });
 } (jQuery, window));
